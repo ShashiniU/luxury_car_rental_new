0726a83d9560323302ba583a99537b5e160f04c4
Shashini.Uwanshika <shashini.uwanshika@softlogic.lk>
2025-02-21 08:52:45 +0530

    commit

---
+++ Backend/server.js
@@ -0,0 +1,41 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// // Connect to MongoDB
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("Error connecting to MongoDB:", err))
+
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err))
+
+// Routes
+const authRoutes = require("./routes/auth")
+const carRoutes = require("./routes/cars")
+
+
+app.use("/api/auth", authRoutes)
+app.use("/api/cars", carRoutes)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
+
